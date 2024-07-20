44c35fccda20eb6a0b060ca6f451fca3fdf45a59
mishal <mishaal79@gmail.com>
2024-07-20 18:55:27 +1000

    add vercel as adapter to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://audiotheft.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: 'server',
+  security: {
+    checkOrigin: true
+  },
+  adapter: vercel()
 });
