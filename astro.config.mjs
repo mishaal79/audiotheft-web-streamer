89e1b3544b8302924cea0a064376b06139a588f9
mishal <mishaal79@gmail.com>
2024-08-17 17:08:34 +1000

    remove recent episodes from hero page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://audiotheft.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon()],
   output: 'hybrid',
   security: {
     checkOrigin: true
   },
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   })
 });
